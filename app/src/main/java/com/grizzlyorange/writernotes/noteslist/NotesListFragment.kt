57f10e4007184647d050ff19f9134393d1fbd6b4
Elena <elena.ivanova.flash@gmail.com>
2021-10-23 11:36:46 +0300

    initial commit

---
+++ app/src/main/java/com/grizzlyorange/writernotes/noteslist/NotesListFragment.kt
@@ -0,0 +1,58 @@
+package com.grizzlyorange.writernotes.noteslist
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.grizzlyorange.writernotes.R
+import com.grizzlyorange.writernotes.databinding.FragmentNotesListBinding
+
+
+class NotesListFragment : Fragment() {
+
+    private var _binding: FragmentNotesListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNotesListBinding.inflate(inflater, container, false)
+
+        binding.fabAddNote.setOnClickListener {
+            moveToDetails()
+        }
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.notes_list_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+                 R.id.btFilterNotes -> {
+                     moveToFilter()
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+    }
+
+    private fun moveToFilter() {
+        findNavController().navigate(
+            R.id.action_notesListFragment_to_notesFilterFragment)
+    }
+
+    private fun moveToDetails() {
+        findNavController().navigate(
+            R.id.action_notesListFragment_to_noteDetailsFragment)
+    }
+}
